a1e6eb8f850d8c91828e0921e604bde1708671e7
beverts <texas312@gmail.com>
2016-03-16 23:43:30 -0500

    add tasks for ansible

---
+++ build/ansible/create-aws-credentials/create.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var fs = require('fs');
+var tl = require('vso-task-lib');
+
+var credsfile = tl.getPathInput('cwd', true) + "credentials.yml";
+var access = "aws_access_key:" + tl.getInput('access', true);
+var secret = "aws_secret_key: " + tl.getInput('secret', true);
+var keypair = "aws_keypair: " + tl.getInput('keypair', true);
+var owner = "aws_owner: " + tl.getInput('owner', true);
+
+var contents = [ '---', '', access, secret, keypair, owner ];
+
+var file = fs.createWriteStream(credsfile);
+file.on('error', function(err) { 
+    tl.err(err);
+    tl.exit(1);
+ });
+contents.forEach(function(v) { 
+    file.write(v + '\n'); 
+});
+file.end();
