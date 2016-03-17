a7086ef7b303ab2192c26e76ede75b155cceab0a
beverts <texas312@gmail.com>
2016-03-17 07:31:55 -0500

    fixed slash and space

--- build/ansible/create-aws-credentials/create.js
+++ build/ansible/create-aws-credentials/create.js
@@ -2,8 +2,8 @@ var path = require('path');
 var fs = require('fs');
 var tl = require('vso-task-lib');
 
-var credsfile = tl.getPathInput('cwd', true) + "credentials.yml";
-var access = "aws_access_key:" + tl.getInput('access', true);
+var credsfile = tl.getPathInput('cwd', true) + "/credentials.yml";
+var access = "aws_access_key: " + tl.getInput('access', true);
 var secret = "aws_secret_key: " + tl.getInput('secret', true);
 var keypair = "aws_keypair: " + tl.getInput('keypair', true);
 var owner = "aws_owner: " + tl.getInput('owner', true);
